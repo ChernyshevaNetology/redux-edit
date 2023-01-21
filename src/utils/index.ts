ab6fb31bf10e0716c7a6454ef086663c0c938f55
Andrew <chernyshevak87@mail.ru>
2023-01-21 12:49:44 +0200

    fixed search handle function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@ export const handleSearch = (str: string, query: string): boolean => {
   const words = str.split(" ");
 
   for (const word of words) {
-    if (word.startsWith(query)) {
+    if (word.toLowerCase().startsWith(query.toLowerCase())) {
       return true;
     }
   }
